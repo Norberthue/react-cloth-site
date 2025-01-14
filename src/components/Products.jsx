078d5ea35430549d7c60e4178a2647cde1486d6c
Norberthue <kokanorbert9@gmail.com>
2025-01-14 09:05:09 +0100

    data for products and render

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
-
+import { useState } from 'react'
+import { PRODUCTS } from '../data/products'
+/**
+ * Products component renders a grid of product items.
+ * Each product displays an image, name, and price.
+ *
+ * @component
+ * @example
+ * return (
+ *   <Products />
+ * )
+ */
 export default function Products() {
-  return (
-    <img src='/assets/images/4.webp'></img>
-  )
+  
+    const [hoveredProductId, setHoveredProductId] = useState(null)
+    console.log(hoveredProductId)
+    return (
+      <div className='flex  justify-center items-center'>
+      <div className='grid grid-cols-1 self-center sm:grid-cols-2 lg:grid-cols-3 gap-y-20 gap-x-10 p-5 sm:p-[50px]'>
+        {PRODUCTS.map((product) => (
+        <div className={`flex-col self-center duration-500 ${hoveredProductId && hoveredProductId !== product.id ? 'blur-md' : ''}`}>
+        <div
+          key={product.id}
+          className={`cursor-pointer`}
+          onMouseEnter={() => setHoveredProductId(product.id)}
+          onMouseLeave={() => setHoveredProductId(null)}
+        >
+          <img className='border-2 border-green-300' src={product.image} alt={product.name} />
+          
+        </div>
+        <p className='font-semibold text-2xl'>{product.name}</p>
+        </div>
+        ))}
+        
+      </div>
+      </div>
+    )
 }
---
+++ src/data/products.js
@@ -0,0 +1,158 @@
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        name: 'UCG DENIM 001 (WASHED BLACK)',
+        price: 150,
+        image: '../assets/images/1.webp',
+        slug: 'UCG-001'  
+    },
+    {
+        id: 2,
+        name: 'UCG DENIM 002 (WASHED BLUE)',
+        price: 160,
+        image: '../assets/images/2.webp',
+        slug: 'UCG-002'
+    },
+    {
+        id: 3,
+        name: 'UCG DENIM 003 (RAW BLACK)',
+        price: 170,
+        image: '../assets/images/3.webp',
+        slug: 'UCG-003'
+    },
+    {
+        id: 4,
+        name: 'UCG DENIM 004 (RAW BLUE)',
+        price: 180,
+        image: '../assets/images/4.webp',
+        slug: 'UCG-004'
+    },
+    {
+        id: 5,
+        name: 'UCG DENIM 005 (WASHED GREY)',
+        price: 150,
+        image: '../assets/images/5.webp',
+        slug: 'UCG-005'
+    },
+    {
+        id: 6,
+        name: 'UCG DENIM 006 (WASHED WHITE)',
+        price: 160,
+        image: '../assets/images/6.webp',
+        slug: 'UCG-006'
+    },
+    {
+        id: 7,
+        name: 'UCG DENIM 007 (RAW GREY)',
+        price: 170,
+        image: '../assets/images/7.webp',
+        slug: 'UCG-007'
+    },
+    {
+        id: 8,
+        name: 'UCG DENIM 008 (RAW WHITE)',
+        price: 180,
+        image: '../assets/images/8.webp',
+        slug: 'UCG-008'
+    },
+    {
+        id: 9,
+        name: 'UCG DENIM 009 (WASHED BLACK)',
+        price: 150,
+        image: '../assets/images/9.webp',
+        slug: 'UCG-009'
+    },
+    {
+        id: 10,
+        name: 'UCG DENIM 010 (WASHED BLUE)',
+        price: 160,
+        image: '../assets/images/10.webp',
+        slug: 'UCG-010'
+    },
+    {
+        id: 11,
+        name: 'UCG DENIM 011 (RAW BLACK)',
+        price: 170,
+        image: '../assets/images/11.webp',
+        slug: 'UCG-011'
+    },
+    {
+        id: 12,
+        name: 'UCG DENIM 012 (RAW BLUE)',
+        price: 180,
+        image: '../assets/images/12.webp',
+        slug: 'UCG-012'
+    },
+    {
+        id: 13,
+        name: 'UCG DENIM 013 (WASHED GREY)',
+        price: 150,
+        image: '../assets/images/13.webp',
+        slug: 'UCG-013'
+    },
+    {
+        id: 14,
+        name: 'UCG DENIM 014 (WASHED WHITE)',
+        price: 160,
+        image: '../assets/images/14.webp',
+        slug: 'UCG-014'
+    },
+    {
+        id: 15,
+        name: 'UCG DENIM 015 (RAW GREY)',
+        price: 170,
+        image: '../assets/images/15.webp',
+        slug: 'UCG-015'
+    },
+    {
+        id: 16,
+        name: 'UCG DENIM 016 (RAW WHITE)',
+        price: 180,
+        image: '../assets/images/16.webp',
+        slug: 'UCG-016'
+    },
+    {
+        id: 17,
+        name: 'UCG DENIM 017 (WASHED BLACK)',
+        price: 150,
+        image: '../assets/images/17.webp',
+        slug: 'UCG-017'
+    },
+    {
+        id: 18,
+        name: 'UCG DENIM 018 (WASHED BLUE)',
+        price: 160,
+        image: '../assets/images/18.webp',
+        slug: 'UCG-018'
+    },
+    {
+        id: 19,
+        name: 'UCG DENIM 019 (RAW BLACK)',
+        price: 170,
+        image: '../assets/images/19.webp',
+        slug: 'UCG-019'
+    },
+    {
+        id: 20,
+        name: 'UCG DENIM 020 (RAW BLUE)',
+        price: 180,
+        image: '../assets/images/20.webp',
+        slug: 'UCG-020'
+    },
+    {
+        id: 21,
+        name: 'UCG DENIM 021 (WASHED GREY)',
+        price: 150,
+        image: '../assets/images/21.webp',
+        slug: 'UCG-021'
+    },
+    {
+        id: 22,
+        name: 'UCG DENIM 022 (WASHED WHITE)',
+        price: 160,
+        image: '../assets/images/22.webp',
+        slug: 'UCG-022'
+    }
+    
+]
